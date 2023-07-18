d542ab9cb5f1c550115ad1ba60882a3b03f7e0cf
arfan <marfanw@gmail.com>
2023-07-18 15:00:28 +0700

    add webcam detection code and update gitignore

---
+++ webcam-object-detection/detect.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+
+# Load YOLO
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("yolov3.txt", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# Set up webcam
+cap = cv2.VideoCapture(0)
+
+# Define output layers and colors
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Preprocessing
+    height, width, channels = frame.shape
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    # Pass the frame through the network
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Process the detections
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    # Apply non-maximum suppression
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+    # Draw bounding boxes and labels
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[i]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, label, (x, y + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+
+    # Display the resulting frame
+    cv2.imshow("Object Detection", frame)
+
+    # Exit the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture and close the window
+cap.release()
+cv2.destroyAllWindows()
